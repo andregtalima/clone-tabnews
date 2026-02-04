827e81f4d06c16d4fee068ed76b3a873b40aec1f
Andre Fabiano <andregtalima@gmail.com>
2026-02-03 21:08:37 -0300

    config BD Neon to production alter with rejectUnauthorized and condicions for local variable

--- infra/database.js
+++ infra/database.js
@@ -1,13 +1,14 @@
 import { Client } from "pg";
 
 async function query(queryObject) {
+  const isLocal = process.env.POSTGRES_HOST === "localhost";
   const client = new Client({
     host: process.env.POSTGRES_HOST,
     port: process.env.POSTGRES_PORT,
     user: process.env.POSTGRES_USER,
     database: process.env.POSTGRES_DB,
     password: process.env.POSTGRES_PASSWORD,
-    ssl: { rejectUnauthorized: false },
+    ssl: isLocal ? false : { rejectUnauthorized: false },
   });
 
   try {
