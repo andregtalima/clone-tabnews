3d70cb8dad6ad66bb72a6f020a62c60b15ef0e35
Andre Fabiano <andregtalima@gmail.com>
2026-02-09 20:56:30 -0300

    adds  endpoints

--- infra/database.js
+++ infra/database.js
@@ -1,18 +1,9 @@
 import { Client } from "pg";
 
 async function query(queryObject) {
-  const isLocal = process.env.POSTGRES_HOST === "localhost";
-  const client = new Client({
-    host: process.env.POSTGRES_HOST,
-    port: process.env.POSTGRES_PORT,
-    user: process.env.POSTGRES_USER,
-    database: process.env.POSTGRES_DB,
-    password: process.env.POSTGRES_PASSWORD,
-    ssl: isLocal ? false : { rejectUnauthorized: false },
-  });
-
+  let client;
   try {
-    await client.connect();
+    client = await getNewClient();
     const result = await client.query(queryObject);
     return result;
   } catch (err) {
@@ -24,5 +15,25 @@ async function query(queryObject) {
 }
 
 export default {
-  query: query,
+  query,
+  getNewClient,
 };
+
+async function getNewClient() {
+  const client = new Client({
+    host: process.env.POSTGRES_HOST,
+    port: process.env.POSTGRES_PORT,
+    user: process.env.POSTGRES_USER,
+    database: process.env.POSTGRES_DB,
+    password: process.env.POSTGRES_PASSWORD,
+    ssl: getSSLValues(),
+  });
+
+  await client.connect();
+  return client;
+}
+
+function getSSLValues() {
+  const isLocal = process.env.POSTGRES_HOST === "localhost";
+  isLocal ? false : { rejectUnauthorized: false };
+}
